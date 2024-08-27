7a2a5a43945e14f722b0e776a6a1d12d95ec7d49
Nicolo’ <120709013+nic005-arch@users.noreply.github.com>
2024-08-27 12:58:05 +0200

    Update cards.py

--- Blackjack/assets/cards.py
+++ Blackjack/assets/cards.py
@@ -1,19 +1,8 @@
 import os
 import requests
 
-# URL base per le carte
-base_url = "https://deckofcardsapi.com/static/img/"
 
-# Seme e valori delle carte
-suits = ['C', 'D', 'H', 'S']
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
-
-# Creare la cartella se non esiste
-if not os.path.exists('cards'):
-    os.makedirs('cards')
-
-# Funzione per scaricare un'immagine
-def download_image(url, filepath):
+def download_immage(url, filepath):
     response = requests.get(url)
     if response.status_code == 200:
         with open(filepath, 'wb') as file:
@@ -22,11 +11,23 @@ def download_image(url, filepath):
     else:
         print(f"Failed to download {url}")
 
-# Scaricare tutte le carte
-for suit in suits:
-    for value in values:
-        card_code = value + suit
-        url = base_url + card_code + ".png"
-        filepath = os.path.join('cards', card_code + ".png")
-        download_image(url, filepath)
-download_immage("https://deckofcardsapi.com/static/img/back.png",os.path.join('cards', "back" + ".png")
+def get_cards(path):
+    base_url = "https://deckofcardsapi.com/static/img/"
+
+    # Seme e valori delle carte
+    suits = ['C', 'D', 'H', 'S']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+
+    # Creare la cartella se non esiste
+    if not os.path.exists(path):
+        os.makedirs('cards')
+
+
+    for suit in suits:
+        for value in values:
+            card_code = value + suit
+            url = base_url + card_code + ".png"
+            filepath = os.path.join('cards', card_code + ".png")
+            download_immage(url, filepath)
+
+    download_immage("https://deckofcardsapi.com/static/img/back.png", filepath=os.path.join('cards', "back" + ".png"))
